1aad3fc95c38ce6aa0bfd6732a9ce6c3b02d91fe
rowshanakero <43588398+rowshanakero@users.noreply.github.com>
2018-11-29 13:01:15 -0600

    Update exam.py

--- exam.py
+++ exam.py
@@ -1,6 +1,10 @@
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict)
+marks = {}.fromkeys(['Math','English','Science'], 0)
+
+# Output: {'English': 0, 'Math': 0, 'Science': 0}
+print(marks)
+
+for item in marks.items():
+    print(item)
+
+# Output: ['English', 'Math', 'Science']
+list(sorted(marks.keys()))
