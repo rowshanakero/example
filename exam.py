65bd9c76c94836060f55e4a8917996355780708f
rowshanakero <43588398+rowshanakero@users.noreply.github.com>
2018-11-29 12:41:44 -0600

    Update exam.py

--- exam.py
+++ exam.py
@@ -1,9 +1,1 @@
-import redis
-
-r = redis.Redis(host='localhost', port=6379, db=0)
-p = r.pipeline()
-for key in keys:
-    p.hgetall(key)
-
-for h in p.execute():
-    print h
+print("Goodbye, World!")
